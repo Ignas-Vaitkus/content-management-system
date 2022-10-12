c7a685056f5449706a37b347bd5032b2fa07cee6
ignas-vaitkus <ignas.vaitkus7@gmail.com>
2022-10-12 19:52:58 +0300

    Added Page entity

---
+++ src/models/Page.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Page;
+
+use Doctrine\ORM\Mapping as ORM;
+use LengthException;
+
+#[ORM\Entity()]
+#[ORM\Table(name: 'pages')]
+class Page
+{
+    #[ORM\Id]
+    #[ORM\Column(type: 'smallint')]
+    #[ORM\GeneratedValue()]
+    protected $id;
+
+    #[ORM\Column(type: 'string')]
+    protected string $title;
+
+    #[ORM\Column(type: 'text')]
+    protected string $content;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    function getName()
+    {
+        return $this->title;
+    }
+    /**
+     * @param string $name 
+     * @return Page
+     */
+    function setTitle(string $title): self
+    {
+        if ($title <= 255) {
+            $this->title = $title;
+            return $this;
+        } else {
+            throw new LengthException('String value must not be larger than 255');
+        }
+    }
+    /**
+     * @return string
+     */
+    function getContent(): string
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param string $content 
+     * @return Page
+     */
+    function setContent(string $content): self
+    {
+        $this->content = $content;
+        return $this;
+    }
+}

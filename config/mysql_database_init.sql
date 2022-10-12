8a583bd23a066a4a2819242c393c969802818784
ignas-vaitkus <ignas.vaitkus7@gmail.com>
2022-10-12 19:50:11 +0300

    Added database and user creation file

---
+++ config/mysql_database_init.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS cms;
+
+CREATE DATABASE cms DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_lithuanian_ci;
+
+DROP USER IF EXISTS 'cms_admin'@'127.0.0.1';
+    
+CREATE USER 'cms_admin'@'127.0.0.1' IDENTIFIED BY 'mysql';
+
+GRANT ALL PRIVILEGES ON cms.* TO 'cms_admin'@'127.0.0.1' WITH GRANT OPTION;

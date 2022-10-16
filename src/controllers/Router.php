3a75d497dab4790fcbd1da1aede43dc798e74438
ignas-vaitkus <ignas.vaitkus7@gmail.com>
2022-10-16 16:35:21 +0300

    Reformatted __construct()

--- src/controllers/Router.php
+++ src/controllers/Router.php
@@ -9,6 +9,14 @@ class Router
     public Request $request;
 
     protected array $routes = [];
+    /**
+     * @param $request Models\Request 
+     * @param $routes array 
+     */
+    function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
     public function get($path, $callback)
     {
         $this->routes['get'][$path] = $callback;
@@ -17,14 +25,11 @@ class Router
     public function resolve()
     {
         $path = $this->request->getPath();
-        echo $path;
-    }
-    /**
-     * @param $request Models\Request 
-     * @param $routes array 
-     */
-    function __construct(Request $request)
-    {
-        $this->request = $request;
+        $method = $this->request->getMethod();
+        $callback = $this->routes[$method][$path];
+
+        echo '<pre>';
+        var_dump($callback);
+        echo '</pre>';
     }
 }

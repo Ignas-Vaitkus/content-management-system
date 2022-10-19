354fe9ce3717587fcc9294a6b2ed6311bf98b357
ignas-vaitkus <ignas.vaitkus7@gmail.com>
2022-10-19 19:16:26 +0300

    Added more routes dinamically from the data base.

--- index.php
+++ index.php
@@ -6,9 +6,20 @@ require('bootstrap.php');
 
 $app = Application::getApp($entityManager, __DIR__);
 
-$app->router->get('/', 'BasicUserNav');
+$app->router->get('/', ['layouts/Main', 'BasicNav']);
 
-$app->router->get('/admin', 'AdminNav');
-$app->router->get('/admin/view', 'AdminNav');
+$pages = Application::getApp()->pages;
+
+foreach ($pages as $page) {
+
+    $app->router->get('/' . $page->getTitle(), ['layouts/Main', 'BasicNav']);
+}
+
+$app->router->get('/Admin', ['layouts/Main', 'layouts/AdminNav', 'CRUD']);
+$app->router->get('/Admin/view', ['layouts/Main', 'layouts/AdminNav', 'BasicNav']);
+
+foreach ($pages as $page) {
+    $app->router->get("/Admin/view/$page", ['layouts/Main', 'layouts/AdminNav', 'BasicNav']);
+}
 
 $app->run();

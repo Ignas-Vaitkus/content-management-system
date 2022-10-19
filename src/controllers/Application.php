362191e2378a4967b9abadcd9d6743f6af5a6a98
ignas-vaitkus <ignas.vaitkus7@gmail.com>
2022-10-19 11:28:11 +0300

    Added response object and reordered class declaration

--- src/controllers/Application.php
+++ src/controllers/Application.php
@@ -5,19 +5,25 @@ namespace Controllers;
 use Models\Request;
 use Controllers\Router;
 use Doctrine\ORM\EntityManager;
+use Models\Response;
 
 class Application
 {
     public static string $rootDir;
-    public EntityManager $entityManager;
+    public static EntityManager $entityManager;
     public Router $router;
     public Request $request;
+    public Response $response;
+    public static Application $app;
     public function __construct(EntityManager $entityManager, string $rootDir)
     {
         self::$rootDir = $rootDir;
+        self::$entityManager = $entityManager;
+        self::$app = $this;
+
+        $this->response = new Response();
         $this->request = new Request();
-        $this->router = new Router($this->request);
-        $this->entityManager = $entityManager;
+        $this->router = new Router($this->request, $this->response);
     }
 
     public function run()

22b61a4ecbb93531c0cd7137ba6bc5656a6be77b
ignas-vaitkus <ignas.vaitkus7@gmail.com>
2022-10-21 16:58:32 +0300

    Delete page functionality.

--- index.php
+++ index.php
@@ -9,12 +9,24 @@ $app = Application::getApp($entityManager, __DIR__, '/Admin');
 $app->router->get('/', ['layouts/Main', 'BasicNav']);
 $app->router->get('/Admin', ['layouts/Main', 'layouts/AdminNav', 'CRUD']);
 $app->router->get('/Admin/view', ['layouts/Main', 'layouts/AdminNav', 'BasicNav']);
+$app->router->get('/Admin/add', ['layouts/Main', 'layouts/AdminNav', 'Create']);
 
 $pages = Application::$pages;
 
 foreach ($pages as $page) {
     $app->router->get('/' . $page->getTitle(), ['layouts/Main', 'BasicNav']);
     $app->router->get('/Admin/view/' . $page->getTitle(), ['layouts/Main', 'layouts/AdminNav', 'BasicNav']);
+    $app->router->get('/Admin/edit/' . $page->getId(), ['layouts/Main', 'Update']);
+}
+
+foreach (array_slice($pages, 1) as $page) {
+    $app->router->post('/Admin/delete/' . $page->getId(), function () use ($page) {
+        //Delete page and redirect
+
+        Application::$entityManager->remove($page);
+        Application::$entityManager->flush();
+        header("Location: /content-management-system/Admin");
+    });
 }
 
 $app->run();

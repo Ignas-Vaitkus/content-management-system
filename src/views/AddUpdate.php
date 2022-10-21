a03a4a056f1a1a80a9870c6be10fb3c575ff06d6
ignas-vaitkus <ignas.vaitkus7@gmail.com>
2022-10-21 19:33:07 +0300

    Added names to form inputs

--- src/views/AddUpdate.php
+++ src/views/AddUpdate.php
@@ -32,15 +32,15 @@ if ($editing) {
     <form action="<?php echo $_SERVER["REQUEST_URI"] ?>" method="POST">
         <div class="mb-3">
             <label for="Title" class="form-label">Title</label>
-            <input type="text" class="form-control" id="Title" value="<?php $editing
-                                                                            ? print $editedPage->getTitle()
-                                                                            : '' ?>">
+            <input type="text" class="form-control" id="Title" name="Title" maxlength="255" value="<?php $editing
+                                                                                                        ? print $editedPage->getTitle()
+                                                                                                        : '' ?>">
         </div>
         <div class="mb-3">
             <label for="Content" class="form-label">Content</label>
-            <textarea class="form-control" id="Content" rows="10"><?php $editing
-                                                                        ? print $editedPage->getContent()
-                                                                        : '' ?></textarea>
+            <textarea class="form-control" id="Content" name="Content" rows="10"><?php $editing
+                                                                                        ? print $editedPage->getContent()
+                                                                                        : '' ?></textarea>
         </div>
         <button type="submit" class="btn btn-primary">
             <?php

9a452965b74e1b5460c091e90df1505e7b0e078a
ignas-vaitkus <ignas.vaitkus7@gmail.com>
2022-10-21 19:34:44 +0300

    Implemented routes for edit and add POST methods

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Controllers\Application;
+use Models\Page;
 
 require('bootstrap.php');
 
@@ -10,6 +11,28 @@ $app->router->get('/', ['layouts/Main', 'BasicNav']);
 $app->router->get('/Admin', ['layouts/Main', 'layouts/AdminNav', 'CRUD']);
 $app->router->get('/Admin/view', ['layouts/Main', 'layouts/AdminNav', 'BasicNav']);
 $app->router->get('/Admin/add', ['layouts/Main', 'layouts/AdminNav', 'AddUpdate']);
+$app->router->post('/Admin/add', function () use ($app) {
+
+    if (
+        isset($_POST['Title'])
+        && isset($_POST['Content'])
+        && strlen($_POST['Title']) <= 255
+        && strlen($_POST['Title']) >= 0
+        && strlen($_POST['Content']) >= 0
+    ) {
+
+        $page = new Page();
+        $page->setTitle($_POST['Title']);
+        $page->setContent($_POST['Content']);
+
+        Application::$entityManager->persist($page);
+        Application::$entityManager->flush();
+        header("Location: /content-management-system/Admin");
+    } else {
+        //Provide more indication to user if data is invalid
+        $app->response->setStatusCode(400);
+    }
+});
 
 $pages = Application::$pages;
 
@@ -17,6 +40,27 @@ foreach ($pages as $page) {
     $app->router->get('/' . $page->getTitle(), ['layouts/Main', 'BasicNav']);
     $app->router->get('/Admin/view/' . $page->getTitle(), ['layouts/Main', 'layouts/AdminNav', 'BasicNav']);
     $app->router->get('/Admin/edit/' . $page->getId(), ['layouts/Main', 'layouts/AdminNav', 'AddUpdate']);
+    $app->router->post('/Admin/edit/' . $page->getId(), function () use ($page, $app) {
+
+        //Move the validation to a function to not repeat the code
+        if (
+            isset($_POST['Title'])
+            && isset($_POST['Content'])
+            && strlen($_POST['Title']) <= 255
+            && strlen($_POST['Title']) >= 0
+            && strlen($_POST['Content']) >= 0
+        ) {
+            $page->setTitle($_POST['Title']);
+            $page->setContent($_POST['Content']);
+
+            Application::$entityManager->persist($page);
+            Application::$entityManager->flush();
+            header("Location: /content-management-system/Admin");
+        } else {
+            //Provide more indication to user that data is invalid
+            $app->response->setStatusCode(400);
+        }
+    });
 }
 
 foreach (array_slice($pages, 1) as $page) {

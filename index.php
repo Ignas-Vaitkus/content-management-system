ebc1f7d8355dcecf32581ea311ff974e6008cede
ignas-vaitkus <ignas.vaitkus7@gmail.com>
2022-10-21 15:09:14 +0300

    Reconfigured routes

--- index.php
+++ index.php
@@ -4,21 +4,16 @@ use Controllers\Application;
 
 require('bootstrap.php');
 
-$app = Application::getApp($entityManager, __DIR__);
+$app = Application::getApp($entityManager, __DIR__, '/Admin');
 
 $app->router->get('/', ['layouts/Main', 'BasicNav']);
-
-$pages = Application::getApp()->pages;
-
-foreach ($pages as $page) {
-
-    $app->router->get('/' . $page->getTitle(), ['layouts/Main', 'BasicNav']);
-}
-
 $app->router->get('/Admin', ['layouts/Main', 'layouts/AdminNav', 'CRUD']);
 $app->router->get('/Admin/view', ['layouts/Main', 'layouts/AdminNav', 'BasicNav']);
 
+$pages = Application::$pages;
+
 foreach ($pages as $page) {
+    $app->router->get('/' . $page->getTitle(), ['layouts/Main', 'BasicNav']);
     $app->router->get('/Admin/view/' . $page->getTitle(), ['layouts/Main', 'layouts/AdminNav', 'BasicNav']);
 }
 
